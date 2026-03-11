2114291cb46138c95f0c7040caf7b73e1ddd55b1
Robert Martin <rodjmartin@gmail.com>
2026-03-11 11:31:07 -0400

    Build: update to use build output environment variable

--- LibraryReact/vite.config.ts
+++ LibraryReact/vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const rodjBuild = process.env.RODJBUILD
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/library/',
+  build: {
+    outDir: rodjBuild ? `${rodjBuild}/paiwares/LibraryReact/dist` : 'dist',
+  },
 })
